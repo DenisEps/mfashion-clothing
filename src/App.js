0630c7b5b20c0f6ab5279802937abec83b9c49e9
DenisEps <denis.epshteyn@gmail.com>
2020-10-11 15:51:45 +0300

    adds shop page, collection preview and collection item components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
+import Shoppage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={Shoppage} />
       </Switch>
     </div>
   );
