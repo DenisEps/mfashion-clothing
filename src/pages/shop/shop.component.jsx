278c73d24233fa9e7f3fa2a78e0ac419942a3911
DenisEps <denis.epshteyn@gmail.com>
2020-10-13 19:45:44 +0300

    moves shop collections to redux and local storage

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-
-import SHOP_DATA from './shop.data';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class Shoppage extends React.Component {
-  constructor(props) {
-    super(props);
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return <div className="shop-page">{collections.map(({id, ...otherCollectionProps}) => (
+const Shoppage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}</div>;
-  }
-}
+    ))}
+  </div>
+);
 
-export default Shoppage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(Shoppage);
