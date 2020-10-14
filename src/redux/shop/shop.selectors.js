8b61468c76d86e483b25d65ad13ba7f542cd7c39
DenisEps <denis.epshteyn@gmail.com>
2020-10-14 10:47:09 +0300

    adds dynamic url routing to collection pages, selecting from redux with match.params.collectionId, memoized collection selector with lodash memoize

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,34 @@
 import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
+
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+};
 
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
+);
+
+// function to use without memoization >>>
+// export const selectCollection = (collectionUrlParam) =>
+//   createSelector([selectCollections], (collections) =>
+//     collections.find(
+//       (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+//     )
+//   );
+
+export const selectCollection = memoize((collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) =>  collections.find(
+      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
+  )
 );
