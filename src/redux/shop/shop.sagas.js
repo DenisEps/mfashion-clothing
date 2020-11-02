6844897f98bba289d32a33f87d1e6276597ab8e9
DenisEps <denis.epshteyn@gmail.com>
2020-11-02 09:59:52 +0300

    replaced fetchcollectionsstart/success/failure with saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,26 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from './shop.actions';
 
 import ShopTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
-  yield console.log('hello');
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
