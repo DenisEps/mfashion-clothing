907c4afe7610b13327e37abd5560f6e0bcfbcafc
DenisEps <denis.epshteyn@gmail.com>
2020-11-11 11:38:40 +0300

    fix shopping bag icon in header

--- client/src/components/cart-icon/cart-icon.styles.jsx
+++ client/src/components/cart-icon/cart-icon.styles.jsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+import { ReactComponent as ShoppingIconSVG } from '../../assets/shopping-bag.svg';
 
 export const CartIconContainer = styled.div`
-  /* width: 45px;
-  height: 45px; */
   width: 45px;
   height: 45px;
   position: relative;
@@ -13,18 +12,14 @@ export const CartIconContainer = styled.div`
   cursor: pointer;
 `;
 
-export const HeaderShoppingIcon = styled(ShoppingIcon)`
-  /* width: 24px;
-  height: 24px; */
-  width: 30px;
-  height: 30px;
+export const HeaderShoppingIcon = styled(ShoppingIconSVG)`
+  width: 24px;
+  height: 24px;
 `;
 
 export const ItemCountContainer = styled.span`
   position: absolute;
   font-size: 10px;
   font-weight: bold;
-  /* bottom: 12px; */
-  bottom: 18px;
-  right: 18px;
+  bottom: 12px;
 `;
